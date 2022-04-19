ba39a9e8ea7c2c00a18e62f3bead97cec7672e20
Urban Krepel <urban.kreppel@gmail.com>
2022-04-19 19:32:41 +0200

    Popravil validatorje za update

--- backend/src/user/user-update.dto.ts
+++ backend/src/user/user-update.dto.ts
@@ -1,11 +1,11 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class UserUpdateDto {
   first_name?: string;
 
   last_name?: string;
 
+  @IsOptional()
+  @IsEmail()
   email?: string;
-
-  password?: string;
 }
